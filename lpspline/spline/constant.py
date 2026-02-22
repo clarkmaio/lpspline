98760dc9b6192243a4f7bf0329cd42d946d1c569
clarkmaio <maioliandrea0@gmail.com>
2026-02-22 11:42:38 +0100

    Add Constant spline component

---
+++ lpspline/spline/constant.py
@@ -0,0 +1,28 @@
+
+
+
+from .base import Spline
+
+
+
+class Constant(Spline):
+    def __init__(self, term: str, tag: Optional[str] = 'constant'):
+        super().__init__(term=term, tag=tag)
+        self._variables = []
+
+    def _build_basis(self, x: np.ndarray) -> np.ndarray:
+        """
+        Builds the Constant basis functions.
+        """
+        return np.ones((len(x), 1))
+
+    def _build_variables(self) -> List[cp.Variable]:
+        """
+        Create cvxpy variables for the spline coefficients.
+        """
+        if not self._variables:
+            self._variables = [cp.Variable(shape=(1,), name=f"{self.term}_constant")]
+        return self._variables
+
+    def __repr__(self):
+        return f"Constant(term='{self.term}')"
